a75b86f7996d772816d2b6b330193b6640c70010
kickindirty <kickindirty@gmail.com>
2013-11-20 12:02:00 -0800

    Changed Arch flag

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,5 +7,6 @@
 source $1
 
 mkdir -p $output_dir
-matlab -nodesktop -nojvm -nosplash -glnx86 -r "run_experiments('$input_dir', '$train_name', '$test_name', $num_train_test_splits, $iterations, $ndcg_rank, $sampling_rate, '$output_dir', '$results_file', $learner, '$input_dir_2', '$train_name_2', '$test_name_2', '$cvx_path')"
+#matlab -nodesktop -nojvm -nosplash -glnx86 -r "run_experiments('$input_dir', '$train_name', '$test_name', $num_train_test_splits, $iterations, $ndcg_rank, $sampling_rate, '$output_dir', '$results_file', $learner, '$input_dir_2', '$train_name_2', '$test_name_2', '$cvx_path')"
+matlab -nodesktop -nojvm -nosplash -r "run_experiments('$input_dir', '$train_name', '$test_name', $num_train_test_splits, $iterations, $ndcg_rank, $sampling_rate, '$output_dir', '$results_file', $learner, '$input_dir_2', '$train_name_2', '$test_name_2', '$cvx_path')"
 
