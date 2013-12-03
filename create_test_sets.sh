ee996f5057591c733064d96b877481ce568c0bd2
Aubrey Gress <adgress@overachiever.(none)>
2013-12-02 17:06:19 -0800

    Fixed script for Overachiever

--- create_test_sets.sh
+++ create_test_sets.sh
@@ -7,6 +7,6 @@
 source $1
 
 mkdir -p $output_dir
-matlab -nodesktop -nojvm -nosplash -glnx86 -r "generate_train_test_split('$fv_file', '$quj_file', $num_quj_columns, $feature_indices_to_use, '$output_dir', $num_train_test_splits, $num_test_queries, $ndcg_rank, '$train_name', '$test_name', $normalize)" 
+matlab -nodesktop -nojvm -nosplash -r "generate_train_test_split('$fv_file', '$quj_file', $num_quj_columns, $feature_indices_to_use, '$output_dir', $num_train_test_splits, $num_test_queries, $ndcg_rank, '$train_name', '$test_name', $normalize)" 
 
 
